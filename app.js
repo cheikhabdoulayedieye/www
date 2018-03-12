14fe128485ab982cd886d229b1310a81109064d7
Yusuf Hasan Miyan <yusufh@protonmail.com>
2018-03-12 18:48:02 +1100

    Added html file creation.

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ const Cookies = require('cookies');
 const PrismicConfig = require('./prismic-configuration');
 const Onboarding = require('./onboarding');
 const app = require('./config');
+const pretty = require('pretty');
+const fs = require('fs');
 
 const PORT = app.get('port');
+const DEPLOY_DIR = app.get('html_output_dir');
+
+let ncp = require('ncp').ncp;
 
 app.listen(PORT, () => {
   Onboarding.trigger();
   process.stdout.write(`Point your browser to: http://localhost:${PORT}\n`);
 });
 
-// Middleware to inject prismic context
+// Middleware to inject prismic context.
 app.use((req, res, next) => {
   res.locals.ctx = {
     endpoint: PrismicConfig.apiEndpoint,
@@ -33,7 +38,37 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.render('homepage');
+  let posts;
+
+  // Get all blog posts.
+  req.prismic.api.query(Prismic.Predicates.at('document.type', 'blog'),{})
+  .then((response) => {
+    posts = response.results;
+  })
+  .catch((error) => {
+    next(`error when retrieving blog posts: ${error.message}`);
+  })
+  .then(() => {
+    res.render('homepage', { posts }, (err, html) => {
+      res.send(html);
+
+      // Write the homepage.
+      const html_file = DEPLOY_DIR + '/index.html';
+      fs.writeFile(html_file, pretty(html), (err) => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('Homepage file was saved.');
+      });
+
+      // Crawl the blog pages.
+      if (posts) {
+        posts.every((post) => {
+          request.get('http://localhost:' + PORT + '/' + post.uid);
+        });
+      }
+    });
+  });
 });
 
 app.get('/:uid', (req, res, next) => {
@@ -41,7 +76,15 @@ app.get('/:uid', (req, res, next) => {
   req.prismic.api.getByUID('blog', uid)
   .then((document) => {
     if (document) {
-      res.render('blog', { document });
+      res.render('blog', { document }, (err, html) => {
+        const html_file = DEPLOY_DIR + '/' + uid + '.html';
+        fs.writeFile(html_file, pretty(html), (err) => {
+          if (err) {
+            return console.log(err);
+          }
+          console.log(`${uid} file was saved.`);
+        });
+      });
     } else {
       res.status(404).send('404 not found');
     }
@@ -52,7 +95,7 @@ app.get('/:uid', (req, res, next) => {
 });
 
 /*
-* Preconfigured prismic preview
+* Preconfigured prismic preview.
 */
 app.get('/preview', (req, res) => {
   const { token } = req.query;
@@ -68,3 +111,39 @@ app.get('/preview', (req, res) => {
     res.send(400, 'Missing token from querystring');
   }
 });
+
+app.createHtmlFiles = function () {
+  // Prepare deploy directory.
+  if (!fs.existsSync(DEPLOY_DIR)) {
+    fs.mkdir(DEPLOY_DIR, (err) => {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
+
+  const crawlPages = () => {
+    // Get the homepage.
+    request.get('http://localhost:' + PORT);
+
+    // Copy public assets.
+    ncp.limit = 16;
+
+    ncp('public', DEPLOY_DIR, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log('Copied all public assets successfully!');
+    });
+  }
+
+  if (fs.existsSync(DEPLOY_DIR)) {
+    // Crawl all pages to generate their files.
+    crawlPages();
+  }
+  else {
+    return console.log(`Could not create deploy dir: DEPLOY_DIR`);
+  }
+};
+
+app.createHtmlFiles();
--- config.js
+++ config.js
@@ -4,7 +4,6 @@
  */
 const express = require('express');
 const favicon = require('serve-favicon');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const errorHandler = require('errorhandler');
@@ -15,10 +14,10 @@ module.exports = (() => {
 
   // all environments
   app.set('port', process.env.PORT || 3000);
+  app.set('html_output_dir', process.env.HTML_OUTPUT_DIR || './deploy');
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'pug');
   app.use(favicon('public/favicon.ico'));
-  app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(methodOverride());
